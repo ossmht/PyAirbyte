29dbcf779df20a8b1f6cedb821853cc19c2484e1
ossmht <111325509+ossmht@users.noreply.github.com>
2024-12-06 09:21:26 +1100

    make kwargs.next_page_token.next_page_token available in self.config.next_page_token to solve https://github.com/airbytehq/airbyte/issues/40697 for now

--- airbyte/caches/__init__.py
+++ airbyte/caches/__init__.py
@@ -3,6 +3,30 @@
 
 from __future__ import annotations
 
+############## HACKS ################
+#
+## HACK
+# put kwargs.next_page_token.next_page_token into self.config.next_page_token. read more in comments below/
+#
+
+from airbyte_cdk.sources.declarative.requesters.request_options.interpolated_nested_request_input_provider import InterpolatedNestedRequestInputProvider
+def hacked_eval_request_inputs(self, stream_state, stream_slice, next_page_token) :
+    kwargs = {
+            "stream_state": stream_state,
+            "stream_slice": stream_slice,
+            "next_page_token": next_page_token,
+        }
+    ##  
+    # HACK is this: put kwargs.next_page_token.next_page_token into self.config.next_page_token, so it can be referenced as config.next_page_token
+    # this solves: https://github.com/airbytehq/airbyte/issues/40697 for now
+    ##
+    self.config['next_page_token'] = kwargs.get('next_page_token').get('next_page_token') if kwargs.get('next_page_token') else None
+    return self._interpolator.eval(self.config, **kwargs)
+    
+InterpolatedNestedRequestInputProvider.eval_request_inputs = hacked_eval_request_inputs
+
+## end of HACK
+
 from airbyte.caches import base, bigquery, duckdb, motherduck, postgres, snowflake, util
 from airbyte.caches.base import CacheBase
 from airbyte.caches.bigquery import BigQueryCache
